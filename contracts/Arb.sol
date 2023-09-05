9a4a0a1517d97499b54a65154f7631c7c6c5da4b
Maheswaran V <78631234+soloking1412@users.noreply.github.com>
2023-09-05 18:58:24 +0530

    Update Arb.sol

--- contracts/Arb.sol
+++ contracts/Arb.sol
@@ -4,28 +4,8 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-interface IERC20 {
-	function totalSupply() external view returns (uint);
-	function balanceOf(address account) external view returns (uint);
-	function transfer(address recipient, uint amount) external returns (bool);
-	function allowance(address owner, address spender) external view returns (uint);
-	function approve(address spender, uint amount) external returns (bool);
-	function transferFrom(address sender, address recipient, uint amount) external returns (bool);
-	event Transfer(address indexed from, address indexed to, uint value);
-	event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-interface IUniswapV2Router {
-  function getAmountsOut(uint256 amountIn, address[] memory path) external view returns (uint256[] memory amounts);
-  function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external returns (uint256[] memory amounts);
-}
-
-interface IUniswapV2Pair {
-  function token0() external view returns (address);
-  function token1() external view returns (address);
-  function swap(uint256 amount0Out,	uint256 amount1Out,	address to,	bytes calldata data) external;
-}
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Arb is Ownable {
 
@@ -36,7 +16,7 @@ contract Arb is Ownable {
 		path[0] = _tokenIn;
 		path[1] = _tokenOut;
 		uint deadline = block.timestamp + 300;
-		IUniswapV2Router(router).swapExactTokensForTokens(_amount, 1, path, address(this), deadline);
+		IUniswapV2Router02(router).swapExactTokensForTokens(_amount, 1, path, address(this), deadline);
 	}
 
 	 function getAmountOutMin(address router, address _tokenIn, address _tokenOut, uint256 _amount) public view returns (uint256) {
@@ -44,7 +24,7 @@ contract Arb is Ownable {
 		path = new address[](2);
 		path[0] = _tokenIn;
 		path[1] = _tokenOut;
-		uint256[] memory amountOutMins = IUniswapV2Router(router).getAmountsOut(_amount, path);
+		uint256[] memory amountOutMins = IUniswapV2Router02(router).getAmountsOut(_amount, path);
 		return amountOutMins[path.length -1];
 	}
 
@@ -85,7 +65,5 @@ contract Arb is Ownable {
 		IERC20 token = IERC20(tokenAddress);
 		token.transfer(msg.sender, token.balanceOf(address(this)));
 	}
-	
-	receive() external payable {}
 
 }
