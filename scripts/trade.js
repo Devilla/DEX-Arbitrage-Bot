4058f8a005c4a8b937c1808332fcf50ff8316611
Maheswaran V <78631234+soloking1412@users.noreply.github.com>
2023-09-05 19:01:29 +0530

    Update trade.js

--- scripts/trade.js
+++ scripts/trade.js
@@ -1,155 +1,53 @@
 const hre = require("hardhat");
 const fs = require("fs");
-require("dotenv").config();
+const dotenv = require("dotenv");
 
-const IERC20_SOURCE = '@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20';
+const config = require("./../config/aurora.json");
+const arb = hre.ethers.contract(config.arb);
+const owner = config.owner;
+let inTrade = false;
 
-
-let config,arb,owner,inTrade,balances;
-const network = hre.network.name;
-console.log(network)
-if (network === 'localhost') config = require('../config/aurora.json');
-if (network === 'fantom') config = require('../config/fantom.json');
-
-console.log(config)
-console.log(`Loaded ${config.routes.length} routes`);
-
-
-
-
-const main = async () => {
-  await setup();
-  // Scale when using own node
-  //[0,0,0,0,0,0,0,0,0].forEach(async (v,i) => {
-  //  await new Promise(r => setTimeout(r, i*1000));
-  //  await lookForDualTrade();
-  //});
-  await lookForDualTrade();
+async function setup() {
+  arb = await arb.connect(owner);
 }
 
-const searchForRoutes = () => {
+async function lookForTriDexTrade() {
   const targetRoute = {};
-  targetRoute.router1 = config.routers[Math.floor(Math.random()*config.routers.length)].address;
-  targetRoute.router2 = config.routers[Math.floor(Math.random()*config.routers.length)].address;
-  targetRoute.token1 = config.baseAssets[Math.floor(Math.random()*config.baseAssets.length)].address;
-  targetRoute.token2 = config.tokens[Math.floor(Math.random()*config.tokens.length)].address;
-  return targetRoute;
-}
+  targetRoute.router1 = config.routers[Math.floor(Math.random() * config.routers.length)].address;
+  targetRoute.router2 = config.routers[Math.floor(Math.random() * config.routers.length)].address;
+  targetRoute.router3 = config.routers[Math.floor(Math.random() * config.routers.length)].address;
+  targetRoute.token1 = config.baseAssets[Math.floor(Math.random() * config.baseAssets.length)].address;
+  targetRoute.token2 = config.tokens[Math.floor(Math.random() * config.tokens.length)].address;
+  targetRoute.token3 = config.tokens[Math.floor(Math.random() * config.tokens.length)].address;
 
-let goodCount = 0;
-const useGoodRoutes = () => {
-  const targetRoute = {};
-  const route = config.routes[goodCount];
-  goodCount += 1;
-  if (goodCount >= config.routes.length) goodCount = 0;
-  targetRoute.router1 = route[0];
-  targetRoute.router2 = route[1];
-  targetRoute.token1 = route[2];
-  targetRoute.token2 = route[3];
-  return targetRoute;
-}
-
-const lookForDualTrade = async () => {
-  let targetRoute;
-  if (config.routes.length > 0) {
-    targetRoute = useGoodRoutes();
-  } else {
-    targetRoute = searchForRoutes();
-  }
   try {
-    let tradeSize = balances[targetRoute.token1].balance;
-    const amtBack = await arb.estimateDualDexTrade(targetRoute.router1, targetRoute.router2, targetRoute.token1, targetRoute.token2, tradeSize);
-    const multiplier = ethers.BigNumber.from(config.minBasisPointsPerTrade+10000);
-    const sizeMultiplied = tradeSize.mul(multiplier);
-    const divider = ethers.BigNumber.from(10000);
-    const profitTarget = sizeMultiplied.div(divider);
-    if (!config.routes.length > 0) {
-      fs.appendFile(`./data/${network}RouteLog.txt`, `["${targetRoute.router1}","${targetRoute.router2}","${targetRoute.token1}","${targetRoute.token2}"],`+"\n", function (err) {});
-    }
-    if (amtBack.gt(profitTarget)) {
-      await dualTrade(targetRoute.router1,targetRoute.router2,targetRoute.token1,targetRoute.token2,tradeSize);
-    } else {
-      await lookForDualTrade();
+    const amtBack = await arb.estimateTriDexTrade(targetRoute.router1, targetRoute.router2, targetRoute.router3, targetRoute.token1, targetRoute.token2, targetRoute.token3, 1);
+
+    if (amtBack.gt(config.minBasisPointsPerTrade)) {
+      return targetRoute;
     }
   } catch (e) {
     console.log(e);
-    await lookForDualTrade();	
   }
+
+  return null;
 }
 
-const dualTrade = async (router1,router2,baseToken,token2,amount) => {
+async function triDexTrade(targetRoute) {
   if (inTrade === true) {
-    await lookForDualTrade();	
-    return false;
+    return;
   }
+
   try {
     inTrade = true;
-    console.log('> Making dualTrade...');
-    const tx = await arb.connect(owner).dualDexTrade(router1, router2, baseToken, token2, amount); //{ gasPrice: 1000000000003, gasLimit: 500000 }
+    console.log('> Making triDexTrade...');
+    const tx = await arb.connect(owner).triDexTrade(targetRoute.router1, targetRoute.router2, targetRoute.router3, targetRoute.token1, targetRoute.token2, targetRoute.token3, 1);
     await tx.wait();
     inTrade = false;
-    await lookForDualTrade();
   } catch (e) {
     console.log(e);
-    inTrade = false;
-    await lookForDualTrade();
   }
 }
-
-const setup = async () => {
-  [owner] = await ethers.getSigners();
-  console.log(`Owner: ${owner.address}`);
-  const IArb = await ethers.getContractFactory('Arb');
-  arb = await IArb.attach(config.arbContract);
-  balances = {};
-  for (let i = 0; i < config.baseAssets.length; i++) {
-    const asset = config.baseAssets[i];
-    const WETH_ADDRESS = "0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB";
-    let interface = await hre.ethers.getContractAt(
-        IERC20_SOURCE,
-        WETH_ADDRESS,
-      );
-    // const interface = await ethers.getContractFactory('WETH9');
-    const assetToken = await interface.attach(asset.address);
-    const balance = await assetToken.balanceOf(config.arbContract);
-    console.log(asset.sym, balance.toString());
-    balances[asset.address] = { sym: asset.sym, balance, startBalance: balance };
-  }
-  setTimeout(() => {
-    setInterval(() => {
-      logResults();
-    }, 600000);
-    logResults();
-  }, 120000);
-}
-
-const logResults = async () => {
-  console.log(`############# LOGS #############`);
-    for (let i = 0; i < config.baseAssets.length; i++) {
-    const asset = config.baseAssets[i];
-    // const interface = await ethers.getContractFactory('WETH9');
-    const WETH_ADDRESS = "0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB";
-    let interface = await hre.ethers.getContractAt(
-        IERC20_SOURCE,
-        WETH_ADDRESS,
-      );
-    const assetToken = await interface.attach(asset.address);
-    balances[asset.address].balance = await assetToken.balanceOf(config.arbContract);
-    const diff = balances[asset.address].balance.sub(balances[asset.address].startBalance);
-    const basisPoints = diff.mul(10000).div(balances[asset.address].startBalance);
-    console.log(`#  ${asset.sym}: ${basisPoints.toString()}bps`);
-  }
-}
-
-process.on('uncaughtException', function(err) {
-  console.log('UnCaught Exception 83: ' + err);
-  console.error(err.stack);
-  fs.appendFile('./critical.txt', err.stack, function(){ });
-});
-
-process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: '+p+' - reason: '+reason);
-});
 
 main()
   .then(() => process.exit(0))
